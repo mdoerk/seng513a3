fc4e05cac887aa4aa01b0373096191d538951130
jjcmoore <jjcm007@hotmail.com>
2011-02-01 22:18:08 -0700

    Added Klingon, with some cleanup. Closes GH-20

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ langs.swedish = require("./langs/swedish");
 langs.quenya = require("./langs/quenya");
 langs.serbian = require("./langs/serbian");
 langs.korean = require("./langs/korean");
-langs.serbian = require("./langs/serbian");
-langs.korean = require("./langs/korean");
 langs.klingon = require("./langs/klingon");
 
 http.createServer(function(request, response) {
